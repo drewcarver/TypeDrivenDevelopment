6092d922e3428871b35be3f8f31139ed93b47fcb
Andrew Carver <acarver@onecallcm.com>
2018-09-18 22:05:09 -0400

    remove abstract class

--- src/types/Email.ts
+++ src/types/Email.ts
@@ -34,49 +34,39 @@ export function matchEmail<TReturn>(email : Email, options : EmailMatchOptions<T
     }
 }
 
-abstract class EmailBase {
-    protected static validateEmail(email: string) : Validation<string[], string> {
+    function validateEmail(email: string) : Validation<string[], string> {
         return validation.Success<string[], string>(email)
-            .concat(this.isMissingAtSign(email))
-            .concat(this.isMissingDomain(email))
-            .concat(this.doesntPassEmailRegex(email));
+            .concat(isMissingAtSign(email))
+            .concat(isMissingDomain(email))
+            .concat(doesntPassEmailRegex(email));
     }
 
-    private static isMissingAtSign(email : string): Validation<string[], string> { 
+    function isMissingAtSign(email : string): Validation<string[], string> { 
         return email.includes('@')
             ? validation.Success(email) 
             : validation.Failure(['Missing @ symbol.']);
     }
 
-    private static isMissingDomain(email : string): Validation<string[], string> { 
+    function isMissingDomain(email : string): Validation<string[], string> { 
         return email.includes('.')
             ? validation.Success(email) 
             : validation.Failure(['Missing domain.']);
     }
 
-    private static doesntPassEmailRegex(email : string) : Validation<string[], string> {
+    function doesntPassEmailRegex(email : string) : Validation<string[], string> {
         const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
         return emailRegex.test(email)
             ? validation.Success(email)
             : validation.Failure(['Doesnt pass the email regex']);
     } 
 
-    public readonly address: string;
-    protected abstract key : string;
-
-    constructor(address: string) {
-        this.address = address;
-    }
-    
-    public abstract match<TReturn>(options : EmailMatchOptions<TReturn>) : TReturn;
-}
-
-export class InvalidEmail extends EmailBase {
+export class InvalidEmail {
     public readonly errors : string[];
     public readonly key = 'Incomplete';
+    public readonly address : string;
 
     constructor(address: string, errors: string[]) {
-        super(address);
+        this.address = address;
         this.errors = errors;
     }
 
@@ -85,21 +75,14 @@ export class InvalidEmail extends EmailBase {
     }
 
 };
-export class InitialEmail extends EmailBase {
+export class InitialEmail {
     public readonly key = 'Initial';
-
-    constructor() {
-        super('');
-    }
-
-    public match<TReturn>(options : EmailMatchOptions<TReturn>) : TReturn {
-        return matchEmail(this, options);
-    }
+    public readonly address = '';
 };
 
-export class ValidEmail extends EmailBase {
+export class ValidEmail {
     public static create(possibleEmail : string) : Email {
-        return this.validateEmail(possibleEmail)
+        return validateEmail(possibleEmail)
             .matchWith<Email>({
                 Failure: errors => new InvalidEmail(possibleEmail, errors.value),
                 Success: () => new ValidEmail(possibleEmail),
@@ -110,11 +93,6 @@ export class ValidEmail extends EmailBase {
     public readonly address: string;
     
     private constructor(email: string) {
-        super(email);
         this.address = email;
-    }
-
-    public match<TReturn>(options : EmailMatchOptions<TReturn>) : TReturn {
-        return matchEmail(this, options);
     }
 }
