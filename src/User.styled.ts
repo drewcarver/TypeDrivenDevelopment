726bbf6843547dc54324584e51da0d00427da1b0
Andrew Carver <drewcarver16@gmail.com>
2018-09-29 14:07:36 -0400

    begin styling with material ui

---
+++ src/User.styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const UserContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
+  width: 50%;
+`;
+
+export const Field = styled.div`
+  padding: 10px, 10px;
+`;
--- src/User.tsx
+++ src/User.tsx
@@ -1,49 +1,85 @@
-import * as React from 'react';
-import { connect } from 'react-redux';
-import { ReducerState } from './redux';
-import * as UserActions from './redux/userReducer';
-import { matchEmail } from './types/Email';
-import { User } from './types/User';
+import {
+  FormControl,
+  FormHelperText,
+  Paper,
+  TextField
+} from "@material-ui/core";
+import * as React from "react";
+import { connect } from "react-redux";
+import { ReducerState } from "./redux";
+import * as UserActions from "./redux/userReducer";
+import { matchEmail } from "./types/Email";
+import { User } from "./types/User";
+import { Field, UserContainer } from "./User.styled";
 
-
-type UserProps = { 
-    user: User
+type UserProps = {
+  user: User;
 };
 
 function withEventValue<TReturnType>(func: (input: string) => TReturnType) {
-    return (event: React.ChangeEvent<HTMLInputElement>) => func(event.target.value);
-};
+  return (event: React.ChangeEvent<HTMLInputElement>) =>
+    func(event.target.value);
+}
 
-const User = (props: UserProps & typeof UserActions) => <div>
-    First Name:
-    <input 
-        value={props.user.name.firstName} 
-        onChange={withEventValue(firstName => props.changeName(firstName, props.user.name.lastName))}
-    />
-    Last Name:
-    <input 
-        value={props.user.name.lastName}
-        onChange={withEventValue(lastName => props.changeName(props.user.name.firstName, lastName))}
-    />
-    Email:
-    <input 
-        value={props.user.email.address} 
-        onChange={withEventValue(props.changeEmail)}
-    />
-    <span>
-        {
-            matchEmail(props.user.email, {
-                Initial: () => '',
-                Invalid: incompleteEmail => incompleteEmail.errors.join('|'),
-                Valid: email => `${email.address} is a valid email!`
-            })
-        }
-    </span>
-    <button onClick={props.save}>Save</button>
-</div>;
+const User = (props: UserProps & typeof UserActions) => (
+  <Paper>
+    <UserContainer>
+      <Field>
+        <TextField
+          label="First Name"
+          value={props.user.name.firstName}
+          onChange={withEventValue(firstName =>
+            props.changeName(firstName, props.user.name.lastName)
+          )}
+        />
+      </Field>
+      <Field>
+        <TextField
+          label="Last Name"
+          value={props.user.name.lastName}
+          onChange={withEventValue(lastName =>
+            props.changeName(props.user.name.firstName, lastName)
+          )}
+        />
+      </Field>
+      <Field>
+        <FormControl>
+          <TextField
+            error={
+              props.user.email.key === "Invalid" &&
+              !!props.user.email.errors.length
+            }
+            label="Email Address"
+            value={props.user.email.address}
+            onChange={withEventValue(props.changeEmail)}
+          />
+          {matchEmail<JSX.Element | string>(props.user.email, {
+            Initial: () => "",
+            Invalid: incompleteEmail => (
+              <React.Fragment>
+                {incompleteEmail.errors.map(error => (
+                  <FormHelperText error>{error}</FormHelperText>
+                ))}
+              </React.Fragment>
+            ),
+            Valid: email => (
+              <FormHelperText>
+                {`${email.address} is a valid email!`}
+              </FormHelperText>
+            )
+          })}
+        </FormControl>
+      </Field>
+      <button onClick={props.save}>Save</button>
+    </UserContainer>
+  </Paper>
+);
 
-const mapStateToProps = (state: ReducerState)  => ({ 
-    user: state.user
+const mapStateToProps = (state: ReducerState) => ({
+  user: state.user
 });
 
-export default connect(mapStateToProps, { ...UserActions })(User);
+export default connect(
+  mapStateToProps,
+  { ...UserActions }
+)(User);
